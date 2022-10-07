b0255d6ec79f6daed02433581151e2238641f854
Yuliya Dzhumanazarova <yaschick451@gmail.com>
2022-10-07 21:38:05 +0300

    update brain_games.py

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 
+from brain_games.cli import welcome_user
+
 def greet(message):
     print(f'{message}')
 
 
 def main():
     greet('Welcome to the Brain Games!')
+    welcome_user()
 
 
 if __name__=='__main__':
