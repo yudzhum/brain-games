4b3b39dcbf0623cc7aefcb534faa230ed06fb2c7
Yuliya Dzhumanazarova <yaschick451@gmail.com>
2022-10-10 14:37:28 +0300

    fix brain_even.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
-from brain_games.brain_even import game_engine
+from brain_games.games.brain_even import brain_even
 
 
 def main():
-    game_engine()
+    brain_even()
 
 
 if __name__ == '__main__':
