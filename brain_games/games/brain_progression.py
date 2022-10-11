a1cf6c0fdd63d3946f6ef98c1264c72722120727
Yuliya Dzhumanazarova <yaschick451@gmail.com>
2022-10-11 16:29:20 +0300

    fix brain_progression

--- brain_games/games/brain_progression.py
+++ brain_games/games/brain_progression.py
@@ -34,28 +34,24 @@ def create_puzzles(progessions):
     """Create dictionary of question-answer pairs"""
     puzzles = {}
 
-    index = 0
-    while index < 3:
-        progression = progessions[index]
+    for progression in progessions:
         # Choose number to hide randomly
         hidden = randint(2, 9)
         # Take answer
-        answer = progression[hidden] 
+        answer = progression[hidden]
         # Hide element
         progression[hidden] = '..'
 
         question = ' '.join([str(x) for x in progression])
 
-        puzzles.update({question : str(answer)})
-
-        index += 1
+        puzzles.update({question: str(answer)})
 
     return puzzles
 
 
 def brain_progression():
     """PLay game of brain progression"""
-    
+
     # Greet user
     name = greeting()
     print('What number is missing in the progression?')
