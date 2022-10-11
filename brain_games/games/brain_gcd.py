f635ec84514ca531ef029c080a33fef52dc4787f
Yuliya Dzhumanazarova <yaschick451@gmail.com>
2022-10-11 15:32:42 +0300

    fix brain_gcd

--- brain_games/games/brain_gcd.py
+++ brain_games/games/brain_gcd.py
@@ -3,48 +3,42 @@ from brain_games.game_engine import game_engine
 from random import randint
 
 
-def create_questions():
-    """Create 3 pairs of numbers"""
-    questions = []
-
-    index = 0
-    while index < 3:
-
-        number1 = randint(1, 100)
-        number2 = randint(1, 100)
-
-        questions.append(f'{number1} {number2}')
-
-        index += 1
-
-    return questions
-
-
-def create_answers(questions):
+def create_puzzles():
     """Create dictionary of 3 question-answer pairs"""
     puzzles = {}
 
     index = 0
     while index < 3:
 
-        # EXtract biggest and smallest numbers from a string
-        num1, num2 = questions[index].split(' ')
-        smallest = min(int(num1), int(num2))
-        biggest = max(int(num1), int(num2))
+        # Create question
+        number1 = randint(1, 100)
+        number2 = randint(1, 100)
 
-        # Find a greatest common divider
-        div_of_smallest = [x for x in range(1, smallest + 1) if smallest % x == 0]
+        question = (f'{number1} {number2}')
 
-        div_of_biggest = [x for x in div_of_smallest if biggest % x == 0]
-
-        divider = str(max(div_of_biggest))
+        # Create answer
+        answer = str(find_gcd(number1, number2))
 
         # Update dictionary
-        puzzles.update({questions[index] : divider})
+        puzzles.update({question: answer})
 
         index += 1
 
     return puzzles
+
+
+def find_gcd(number1, number2):
+    """"Find greatest common divider"""
+
+    smallest = min(number1, number2)
+    biggest = max(number1, number2)
+
+    # Find a greatest common divider
+    div_of_smallest = [x for x in range(1, smallest + 1) if smallest % x == 0]
+
+    div_of_biggest = [x for x in div_of_smallest if biggest % x == 0]
+
+    return max(div_of_biggest)
 
 
 def brain_gcd():
@@ -53,8 +47,7 @@ def brain_gcd():
     print('Find the greatest common divisor of given numbers.')
 
     # Cteate questions and answer
-    questions = create_questions()
-    puzzles = create_answers(questions)
+    puzzles = create_puzzles()
 
     # Play game
     game_engine(name, puzzles)
