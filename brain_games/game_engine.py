3494d741b2006937a11ad1517febb204f8e267ea
Yuliya Dzhumanazarova <yaschick451@gmail.com>
2022-10-17 21:02:22 +0300

    fix game_engine

--- brain_games/game_engine.py
+++ brain_games/game_engine.py
@@ -1,32 +1,14 @@
 import prompt
-import brain_games.games.brain_prime
-import brain_games.games.brain_calc
-import brain_games.games.brain_even
-import brain_games.games.brain_gcd
-import brain_games.games.brain_progression
 
 
-# Game rules dictionary
-rules = {
-    'brain-even': 'Answer "yes" if the number is even, otherwise answer "no".',
-    'brain-gcd': 'Find the greatest common divisor of given numbers.',
-    'brain-calc': 'What is the result of the expression?',
-    'brain-progression': 'What number is missing in the progression?',
-    'brain-prime':
-        'Answer "yes" if given number is prime. Otherwise answer "no".',
-}
+FIRST_ROUND = 0
+FINAL_ROUND = 3
 
-# Dictionary with game round generating functions
-rounds = {
-    'brain-even': brain_games.games.brain_even.generate_round,
-    'brain-gcd': brain_games.games.brain_gcd.generate_round,
-    'brain-calc': brain_games.games.brain_calc.generate_round,
-    'brain-progression': brain_games.games.brain_progression.generate_round,
-    'brain-prime': brain_games.games.brain_prime.generate_round
-}
+START_SCORE = 0
+MAX_SCORE = 3
 
 
-def game_engine(game):
+def run_game(game):
     """Greet user. Print rules.
     Ask question 3 times. Compare answer with user guess,
     if answer is correct, then ask again.
@@ -39,16 +21,13 @@ def game_engine(game):
     print(f'Hello, {name}')
 
     # Print rules
-    print(f'{rules[game]}')
+    print(f'{game.DESCRIPTION}')
 
-    # What game would be playing
-    get_round = rounds[game]
-
-    index = 0
-    while index < 3:
+    score = START_SCORE
+    for _ in range(FIRST_ROUND, FINAL_ROUND):
 
         # Get game round
-        question, answer = get_round()
+        question, answer = game.generate_round()
 
         # Ask user a question
         print(f'Question: {question}')
@@ -59,7 +38,7 @@ def game_engine(game):
         # Copmare user guess and answer
         if answer == guess:
             print('Correct!')
-            index += 1
+            score += 1
         else:
             print(f"'{guess}' is wrong answer ;(. ", end='')
             print(f"Correct answer was '{answer}'.")
@@ -67,14 +46,5 @@ def game_engine(game):
             break
 
     # User gave 3 correct answer
-    if index == 3:
+    if score == MAX_SCORE:
         print(f'Congratulations, {name}!')
-
-
-def main():
-    game = 'brain-prime'
-    game_engine(game)
-
-
-if __name__ == '__main__':
-    main()
